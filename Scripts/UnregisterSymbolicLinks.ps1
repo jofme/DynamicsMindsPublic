1b13f4b6192359c5087ceb8087ccb056b8a4d40a
Jonas Melgaard <jonas@melgaard.nu>
2025-05-12 16:38:22 +0000

    Initialize repository

---
+++ Scripts/UnregisterSymbolicLinks.ps1
@@ -0,0 +1,8 @@
+﻿$AOSMetadataPath = "C:\AOSService\PackagesLocalDirectory"
+$RepoPath = ".."
+$RepoMetadataPath = $RepoPath + "\Metadata"
+$RepoModelFolders = Get-ChildItem $RepoMetadataPath -Directory
+foreach ($ModelFolder in $RepoModelFolders)
+{
+	cmd /c rmdir "$AOSMetadataPath\$ModelFolder"
+}
